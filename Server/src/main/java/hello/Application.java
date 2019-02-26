43ace24fa78cf5041f38e6e2ee9ba729dc6cc740
Ceren U_urlu <c.ugurlu@student.tudelft.nl>
2019-02-26 07:56:07 +0000

    Revert "Merge branch 'master' of https://gitlab.ewi.tudelft.nl/cse1105/2018-2019/oopp-group-43/template"
    This reverts commit 4d09f82aecce025b2c5240445d600664d6d810b5

--- Server/src/main/java/hello/Application.java
+++ Server/src/main/java/hello/Application.java
@@ -3,11 +3,9 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//application to boot with spring
 @SpringBootApplication
 public class Application {
 
-    //run the application
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
