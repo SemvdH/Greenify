2e1becbe329f90593e352bdaacbd8a27abae007d
Sem van der Hoeven <s.vanderhoeven@student.tudelft.nl>
2019-03-09 14:12:44 +0100

    REMOVE::removed settings.gradle file in src folder
    this messed with the paths for the main settings.gradle file

--- src/Server/src/main/java/gogreen/server/data/model/User.java
+++ src/Server/src/main/java/gogreen/server/data/model/User.java
@@ -1,8 +1,8 @@
 package gogreen.server.data.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 
 @Entity
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
