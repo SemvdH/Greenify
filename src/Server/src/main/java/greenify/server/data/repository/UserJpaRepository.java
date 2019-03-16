98df90b0b2e3a3181f7448f2f6c5db3e8b8bc3e4
Ceren Ugurlu <c.ugurlu@student.tudelft.nl>
2019-03-16 13:27:07 +0000

    Delete UserJpaRepository.java

--- src/Server/src/main/java/greenify/server/data/repository/UserJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package greenify.server.data.repository;
-
-import greenify.server.data.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends UserRepository, JpaRepository<User,Long> {
-
-}
