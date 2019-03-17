7539dd20f629792d509de2bed285c309eb3cafd6
Sem van der Hoeven <s.vanderhoeven@student.tudelft.nl>
2019-03-17 13:09:27 +0100

    EDIT::changed GUI name to greenify

--- src/Server/src/main/java/greenify/server/data/repository/UserRepository.java
+++ src/Server/src/main/java/greenify/server/data/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package greenify.server.data.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import greenify.server.data.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByName(String name);
+    User findByName(@Param("name") String name);
+    //    User findByName(String name);
     <T extends User> T save(T user);
 }
 
