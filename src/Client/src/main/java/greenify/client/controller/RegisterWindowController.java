dfdbc8d5a92dccf81cc0cce265746f15e450920e
Sem van der Hoeven <s.vanderhoeven@student.tudelft.nl>
2019-03-21 10:45:54 +0100

    ADD::added friends pane and button to dashboard
    ADD::added load animations to register window

--- src/Client/src/main/java/greenify/client/controller/RegisterWindowController.java
+++ src/Client/src/main/java/greenify/client/controller/RegisterWindowController.java
@@ -1,16 +1,23 @@
 package greenify.client.controller;
 
 import greenify.client.rest.UserService;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.concurrent.TimeUnit;
+
 //class that controls the actions for the register window
 @Controller
 public class RegisterWindowController {
@@ -26,6 +33,25 @@ public class RegisterWindowController {
     private PasswordField passwordField2;
     @FXML
     private Button signupButton;
+    @FXML
+    private Line uNamePathLine;
+
+    public void initialize() throws InterruptedException {
+        //        PathTransition pathTransUName = new PathTransition(Duration.millis(1100), uNamePathLine, userNameText);
+        //        pathTransUName.play();
+        addSlideAnimation(1100, userNameText, -300);
+        addSlideAnimation(1100, passwordField, 300);
+        TimeUnit.MILLISECONDS.sleep(300);
+        addSlideAnimation(1100, passwordField2, -420);
+
+    }
+
+    public void addSlideAnimation(int duration, Node node, int from) {
+        TranslateTransition slideIn = new TranslateTransition(Duration.millis(duration), node);
+        slideIn.setFromX(from);
+        slideIn.setToX(0);
+        slideIn.play();
+    }
 
     /**
      * signs the user up.
