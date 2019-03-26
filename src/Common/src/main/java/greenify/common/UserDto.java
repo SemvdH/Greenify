61e31df1578abe97a06118b6b382bcff87087136
mlwauben <m.l.wauben@student.tudelft.nl>
2019-03-26 08:46:54 +0100

    FIX:: checkstyle 'common'

--- src/Common/src/main/java/greenify/common/UserDto.java
+++ src/Common/src/main/java/greenify/common/UserDto.java
@@ -1,9 +1,13 @@
+/*
+  DTO stands for Data Transfer Object.
+  is an object that carries data between processes.
+  The motivation for its use is that communication between processes
+  is usually done
+  resorting to remote interfaces (e.g., web services),
+  where each call is an expensive operation.
+ */
 package greenify.common;
 
-// DTO stands for Data Transfer Object.
-// is an object that carries data between processes.
-// The motivation for its use is that communication between processes is usually done
-// resorting to remote interfaces (e.g., web services), where each call is an expensive operation.
 public class UserDto {
     private Long id;
     private String name;
@@ -21,18 +25,34 @@ public class UserDto {
         this.name = name;
     }
 
+    /**
+     * Method that returns the name of a user.
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method that sets the name of a user.
+     * @param name name of a user
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Method that returns the ID of a user.
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Method that sets the ID of a user.
+     * @param id id of a user
+     */
     public void setId(Long id) {
         this.id = id;
     }
