1f12291398ac169086685f4a35cef40b7df89706
Merel Steenbergen <m.a.steenbergen@student.tudelft.nl>
2019-04-04 21:00:37 +0200

    FIX::Import error

--- src/Client/src/main/java/greenify/client/controller/CalculatorController.java
+++ src/Client/src/main/java/greenify/client/controller/CalculatorController.java
@@ -14,6 +14,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
